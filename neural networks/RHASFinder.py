f84f31ac79d99d4ea808999126519f701727952a
Celesteyh <127289307+Celesteyh@users.noreply.github.com>
2025-01-17 12:58:02 +1100

    Add the new version of RHASFinder

--- neural networks/RHASFinder.py
+++ neural networks/RHASFinder.py
@@ -10,7 +10,7 @@ def fc_relu_dropout(in_channels: int, out_channels: int, dropout: float) -> nn.S
     )
 
 
-def conv2_bn_relu(in_channels: int, out_channels: int, kernel_size, padding='same'):
+def conv2_bn_relu(in_channels: int, out_channels: int, kernel_size, padding='same') -> nn.Sequential:
     return nn.Sequential(
         nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding),
         nn.BatchNorm2d(out_channels),
@@ -53,3 +53,26 @@ class Conv2d(nn.Module):
         return x
 
 # number of parameters = 193171
+
+# new RHASFinder
+class EncoderBlock(nn.Module):
+    def __init__(self, input_dim=22, num_classes=4, num_heads=1, num_layers=4, dim_feedforward=128):
+        super(EncoderBlock, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 64)
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=64,
+            nhead=num_heads,
+            dim_feedforward=dim_feedforward
+        )
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
+        self.fc2 = nn.Linear(64, num_classes)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = x.permute(1, 0, 2)
+        x = self.transformer_encoder(x)
+        x = x.mean(dim=0)
+        x = self.fc2(x)
+        return x
+        
+# number of parameters = 135620
